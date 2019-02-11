ece468ea760bbc9e40192202a93773dddac30310
Jeremy Huff <jeremythuff@gmail.com>
2019-02-10 23:07:15 -0600

    Added advanced sessions

---
+++ Source/GamePlayTest/GamePlayTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GamePlayTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYTEST_API AGamePlayTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GamePlayTest/ShipBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicle.h"
+#include "ShipBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYTEST_API AShipBase : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+};
